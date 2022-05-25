ce9cd3eb69e6fb2e9263b219ae851d90321944eb
sprchu <281798865@qq.com>
2022-05-25 20:30:56 +0800

    feat(servebase): add common error handling module

---
+++ servebase/errno/errno.go
@@ -0,0 +1,38 @@
+package errno
+
+const (
+	SuccessCode    = "0"
+	ServiceErrCode = "10001"
+	ParamErrCode   = "10002"
+	AuthErrCode    = "10003"
+
+	LoginErrCode            = "20001"
+	UserNotExistErrCode     = "20002"
+	UserAlreadyExistErrCode = "20003"
+	RegisterErrCode         = "20004"
+	GetUserInfoErrCode      = "20005"
+)
+
+type ErrNo struct {
+	ErrCode string
+	ErrMsg  string
+}
+
+func (e ErrNo) Error() string {
+	return e.ErrMsg
+}
+
+func NewErrNo(code string, msg string) error {
+	return ErrNo{code, msg}
+}
+
+var (
+	Success    = NewErrNo(SuccessCode, "Success")
+	ServiceErr = NewErrNo(ServiceErrCode, "Service is unable to start successfully")
+	ParamErr   = NewErrNo(ParamErrCode, "Wrong Parameter has been given")
+	AuthErr    = NewErrNo(AuthErrCode, "Invalid token")
+
+	LoginErr            = NewErrNo(LoginErrCode, "Wrong username or password")
+	UserNotExistErr     = NewErrNo(UserNotExistErrCode, "User does not exists")
+	UserAlreadyExistErr = NewErrNo(UserAlreadyExistErrCode, "User already exists")
+)
