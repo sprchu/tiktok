b85bc99274c391fb0e84cf431ecc7313c4b3a9b2
sprchu <281798865@qq.com>
2022-05-28 22:43:07 +0800

    feat(videomgr): add rpc module

---
+++ videomgr/rpc/internal/logic/publishactionlogic.go
@@ -0,0 +1,38 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/ByteDance-camp/TickTalk/videomgr/model"
+	"github.com/ByteDance-camp/TickTalk/videomgr/rpc/internal/svc"
+	"github.com/ByteDance-camp/TickTalk/videomgr/rpc/types/videomanager"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PublishActionLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewPublishActionLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PublishActionLogic {
+	return &PublishActionLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *PublishActionLogic) PublishAction(in *videomanager.PublishActionRequest) (*videomanager.PublishActionResponse, error) {
+	_, err := l.svcCtx.VideoModel.Insert(l.ctx, &model.Video{
+		Title:   in.Title,
+		FileUrl: in.Url,
+		UserId:  in.UserId,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &videomanager.PublishActionResponse{}, nil
+}
---
+++ videomgr/rpc/internal/logic/publishlistlogic.go
@@ -0,0 +1,49 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/ByteDance-camp/TickTalk/user/rpc/types/user"
+	"github.com/ByteDance-camp/TickTalk/videomgr/rpc/internal/svc"
+	"github.com/ByteDance-camp/TickTalk/videomgr/rpc/types/videomanager"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PublishListLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewPublishListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PublishListLogic {
+	return &PublishListLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *PublishListLogic) PublishList(in *videomanager.PublishListRequest) (*videomanager.PublishListResponse, error) {
+	videos, err := l.svcCtx.VideoModel.GetByUser(l.ctx, in.UserId)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]*videomanager.Video, 0, len(videos))
+	for _, video := range videos {
+		resp, err := l.svcCtx.UserRpc.UserInfo(l.ctx, &user.UserInfoRequest{
+			UserId: video.UserId,
+		})
+		if err != nil {
+			return nil, err
+		}
+		vd := videoResolver(&video)
+		vd.Author = userInfoResolver(resp.UserInfo)
+		res = append(res, vd)
+	}
+
+	return &videomanager.PublishListResponse{
+		VideoList: res,
+	}, nil
+}
