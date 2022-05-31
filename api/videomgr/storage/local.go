a3451aac3e10e7950424b0a97c9a7415c6b6b9ca
sprchu <281798865@qq.com>
2022-05-31 14:16:20 +0800

    refactor(api/videomgr): add function to generate cover in local storage

--- api/videomgr/storage/local.go
+++ api/videomgr/storage/local.go
@@ -6,15 +6,18 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"io"
+	"io/fs"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type localHandler struct {
+	addr string
 	path string
 }
 
@@ -22,51 +25,47 @@ func NewLocalHandler(addr, path string) (*localHandler, chan struct{}) {
 	stop := make(chan struct{})
 	h := http.FileServer(http.Dir(path))
 	go func() {
-		err := http.ListenAndServe(addr, h)
+		err := http.ListenAndServe(addr[strings.Index(addr, ":"):], h)
 		if err != nil {
 			logx.Errorf("file server error: %v", err)
 			stop <- struct{}{}
 		}
 	}()
-	return &localHandler{path: path}, stop
+	return &localHandler{addr: addr, path: path}, stop
 }
 
-func (h *localHandler) Upload(ctx context.Context, file *multipart.FileHeader) (string, error) {
+func (h *localHandler) Upload(ctx context.Context, file *multipart.FileHeader) (string, string, error) {
 	f, err := file.Open()
 	if err != nil {
 		logx.Errorf("open multipart file error: %w", err)
-		return "", err
+		return "", "", err
 	}
 	defer f.Close()
 
 	content, err := ioutil.ReadAll(f)
 	if err != nil {
 		logx.Errorf("read multipart file error: %w", err)
-		return "", err
+		return "", "", err
 	}
 
 	sha := sha256.Sum256(content)
 	storeFileName := fmt.Sprintf("%s/%x", h.path, sha)
-	_, err = os.Stat(storeFileName)
-	if err == nil {
-		return storeFileName, nil
-	}
-	if !os.IsExist(err) {
-		logx.Errorf("failed to read file info: %w, sha256: %x", err, sha)
-		return "", err
+	ok := fs.ValidPath(storeFileName)
+	if ok {
+		return storeFileName, storeFileName + ".png", nil
 	}
 
-	storeFile, err := os.Open(storeFileName)
+	storeFile, err := os.Create(storeFileName)
 	if err != nil {
-		logx.Errorf("failed to open file to store: %w, sha256: %x", err, sha)
-		return "", err
+		logx.Errorf("failed to create file: %w, sha256: %x", err, sha)
+		return "", "", err
 	}
 	defer storeFile.Close()
 	_, err = io.Copy(storeFile, bytes.NewBuffer(content))
 	if err != nil {
 		logx.Errorf("write file error: %v", err)
-		return "", err
+		return "", "", err
 	}
 
-	return fmt.Sprintf("%s/%x", h.path, sha), nil
+	return fmt.Sprintf("%s/%x", h.addr, sha), "https://xcj-pic.oss-cn-beijing.aliyuncs.com/kedaya.jpg", nil
 }
--- api/videomgr/storage/storage.go
+++ api/videomgr/storage/storage.go
@@ -10,17 +10,17 @@ var handler FileHandler = &nilHandler{}
 type nilHandler struct{}
 
 type FileHandler interface {
-	Upload(ctx context.Context, file *multipart.FileHeader) (url string, err error)
+	Upload(ctx context.Context, file *multipart.FileHeader) (url string, cover string, err error)
 }
 
 func RegisterHandler(hd FileHandler) {
 	handler = hd
 }
 
-func Upload(ctx context.Context, file *multipart.FileHeader) (url string, err error) {
+func Upload(ctx context.Context, file *multipart.FileHeader) (url string, cover string, err error) {
 	return handler.Upload(ctx, file)
 }
 
-func (h *nilHandler) Upload(ctx context.Context, file *multipart.FileHeader) (string, error) {
-	return "create by nilHandler", nil
+func (h *nilHandler) Upload(ctx context.Context, file *multipart.FileHeader) (string, string, error) {
+	return "create by nilHandler", "no cover", nil
 }
