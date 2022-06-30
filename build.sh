45ddc01cfe8b9dca32ded5be82d355b2222ff1ba
sprchu <281798865@qq.com>
2022-06-30 19:10:16 +0800

    chore: add deployment code for social module

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ go mod tidy
 
 go build -v -o ./user/main ./user/rpc/user.go
 go build -v -o ./videomgr/main ./videomgr/rpc/videomanager.go
+go build -v -o ./social/main ./social/rpc/social.go
 go build -v -o ./api/main ./api/main.go
