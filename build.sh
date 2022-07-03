4d64d23a4b5c41064fd6e1daa4d0b7bdd6298d80
sprchu <281798865@qq.com>
2022-07-03 14:51:26 +0800

    chore: remove useless file or code

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-go mod tidy
-
-go build -v -o ./user/main ./user/rpc/user.go
-go build -v -o ./videomgr/main ./videomgr/rpc/videomanager.go
-go build -v -o ./social/main ./social/rpc/social.go
-go build -v -o ./api/main ./api/main.go
